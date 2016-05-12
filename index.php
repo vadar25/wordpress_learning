adfd5dce1fadd19178161ee1abc00043fa031c5b
vadar <szekelyferenc25@gmail.com>
2016-05-13 00:39:11 +0200

    Permalink
    Adding the Posts as links

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 if (have_posts()) :
 	while (have_posts()) : the_post();?>
-	<h2><?php echo the_title()?></h2>
+	<h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
   <?php the_content();?>
 <?php 	endwhile;
 	else :
