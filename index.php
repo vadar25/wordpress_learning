ca6e3898aba1c7512fad5ca1d5ac21e24042d162
vadar <szekelyferenc25@gmail.com>
2016-05-12 17:45:32 +0200

    Printing post
    Dynamically printing out posts on the webpage

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<h1>Hello World</h1>
+<?php
+
+if (have_posts()) :
+	while (have_posts()) : the_post();?>
+	<h2><?php echo the_title()?></h2>
+  <?php the_content();?>
+<?php 	endwhile;
+	else :
+		echo '<p>No content found.</p>';
+	endif;
+
+?>
