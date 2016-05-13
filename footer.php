0341f64f41844e12ea3930ca663f1e174dd94022
vadar <szekelyferenc25@gmail.com>
2016-05-13 13:21:17 +0200

    styling the header/footer
    adding max width to the site and some borders to header/footer

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-<footer class="site-footer">
+  <footer class="site-footer">
 
-  <p><?php bloginfo('name');?> - &copy <?php echo date('Y');?></p>
-</footer>
+    <p><?php bloginfo('name');?> - &copy <?php echo date('Y');?></p>
+  </footer>
+</div><!-- container-->
 <?php wp_footer();?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -8,8 +8,8 @@
   </head>
 
 <body <?php body_class();?>>
-
-  <header class="site-header">
-    <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a>  </h1>
-    <h5><?php bloginfo('description');?>  </h5>
-  </header>
+  <div class="container">
+    <header class="site-header">
+      <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a>  </h1>
+      <h5><?php bloginfo('description');?>  </h5>
+    </header>
