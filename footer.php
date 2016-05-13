d66b2e83d1add3915aa496242228c3e1de058120
vadar <szekelyferenc25@gmail.com>
2016-05-13 12:46:36 +0200

    adding elemnts to the footer
    some info to the footer to look normal

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
+<footer class="site-footer">
 
+  <p><?php bloginfo('name');?> - $copy <?php echo date('Y');?></p>
+</footer>
+<?php wp_footer();?>
 </body>
 </html>
-  
--- header.php
+++ header.php
@@ -10,6 +10,6 @@
 <body <?php body_class();?>>
 
   <header class="site-header">
-    <h1><?php bloginfo('name');?>  </h1>
+    <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a>  </h1>
     <h5><?php bloginfo('description');?>  </h5>
   </header>
