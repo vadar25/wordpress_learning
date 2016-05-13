c773c0f9483133345e237108225e4e530e0caad7
vadar <szekelyferenc25@gmail.com>
2016-05-13 13:48:23 +0200

    Final styling on index.php
    few additional styling on index.php end of lesson

--- index.php
+++ index.php
@@ -6,9 +6,10 @@ get_header();
 
 if (have_posts()) :
 	while (have_posts()) : the_post();?>
-	<h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
-  <?php the_content();?>
-
+	<article class="post">
+		<h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
+	  <?php the_content();?>
+	</article>
 <?php 	endwhile;
 	else :
 		echo '<p>No content found.</p>';
