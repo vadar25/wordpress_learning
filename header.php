eb23143f76c34e00a9e5585925043b9310714ce2
vadar <szekelyferenc25@gmail.com>
2016-05-14 00:24:19 +0200

    last official change

--- header.php
+++ header.php
@@ -11,7 +11,11 @@
   <div class="container">
     <header class="site-header">
       <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a>  </h1>
-      <h5><?php bloginfo('description');?>  </h5>
+      <h5><?php bloginfo('description');?><?php if (is_page('Home')) {?>
+         Thanks for watching this.
+      <?php } ?>  </h5>
+
+      <?php if (is_page()) ?>
       <nav class="site-nav">
         <?php
         $args = array('theme_location' => 'primary');
