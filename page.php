2d9be715a5572cd59dde7706a6a1385661f60995
vadar <szekelyferenc25@gmail.com>
2016-05-13 23:42:14 +0200

    CSS changes
    Make some more changes to style.css

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+
+get_header();
+
+if (have_posts()) :
+	while (have_posts()) : the_post();
+	if(!in_category('utazas')) :?>
+	<article class="post page">
+		<h2><?php echo the_title()?></h2>
+	  <?php the_content();?>
+	</article>
+<?php 	endif; 	endwhile;
+	else :
+		echo '<p>No content found.</p>';
+	endif;
+get_footer();
+?>
