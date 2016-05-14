189a9a7c163ab170ca8d8dc664acf86e3e1e8f40
vadar <szekelyferenc25@gmail.com>
2016-05-14 13:44:31 +0200

    Positioning the pages
    Adding aside and position it with the article elements

--- page.php
+++ page.php
@@ -3,7 +3,23 @@
 get_header();
 
 query_posts("cat=Home");
-$page_title = get_the_title();
+$page_title = get_the_title();?>
+<div id="content-container">
+	<aside id="side_news">
+		<h4>Heti újdonságok:</h4>
+				<ul>
+				<li>Julinak nem sikerült kihúznia a vikingek ősi kardját, de legalább látta.
+				</li>
+				<li>Julit nem csak konduktorként, de fényképészként is alkalmaznák Stavangerben. Ezért jó lenne, ha levenné a cenzűrát és az oldalra is feltölthetnék néhány képet.
+				</li>
+				<li>Engem regisztráltak mint munkakeresőt Norvégiában. Gyakorlatilag egy könyvtárnyi papírt kellett beszereznem hivatalosan,
+				ami a gyakorlatban így zajlott. Kérem az útlevelét, köszönöm... Legalább pipa.
+				</li>
+				<li>Hivatalosan is a sakk-klub tagja lettem trondheim-ben. Beneveztem egy versenyre is. Már az első forduló is megvolt, de ez a következő heti hirekhez tartozik.
+				</li>
+				</ul>
+	</aside>
+<?php
 if (have_posts()) :
 	while (have_posts()) : the_post();
 	if(in_category($page_title)) :?>
@@ -17,7 +33,9 @@ if (have_posts()) :
 <?php 	endif; 	endwhile;
 	else :
 		echo '<p>No content found.</p>';
-	endif;
-  wp_reset_query();
+	endif;?>
+
+</div>
+<?php	  wp_reset_query();
 get_footer();
 ?>
