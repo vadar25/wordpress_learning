90e908c59d0814d985a233d6a017c2eca23debca
vadar <szekelyferenc25@gmail.com>
2016-05-17 00:35:04 +0200

    Setting the nav
    Setting up the navigation menu bar

--- header.php
+++ header.php
@@ -9,6 +9,12 @@
 
 <body <?php body_class();?>>
   <div class="container">
+    <nav class="site-nav">
+      <?php
+      $args = array('theme_location' => 'primary');
+      ?>
+      <?php wp_nav_menu($args);?>
+    </nav>
     <header class="site-header">
       <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a>  </h1>
       <h5><?php bloginfo('description');?><?php if (is_page('Home')) {?>
@@ -16,10 +22,4 @@
       <?php } ?>  </h5>
 
       <?php if (is_page()) ?>
-      <nav class="site-nav">
-        <?php
-        $args = array('theme_location' => 'primary');
-        ?>
-        <?php wp_nav_menu($args);?>
-      </nav>
     </header>
