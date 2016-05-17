13386886eda1f418c88c2849eb0316051d985e1f
vadar <szekelyferenc25@gmail.com>
2016-05-18 01:02:33 +0200

    Footer done
    Final footer elements are set

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
   <footer class="site-footer">
+    <p><?php echo "VadarBeta";?> - &copy <?php echo "2016";?></p>
     <nav class="site-nav-footer">
       <?php
       $args = array('theme_location' => 'footer');
       ?>
       <?php wp_nav_menu($args);?>
     </nav>
-    <p><?php bloginfo('name');?> - &copy <?php echo date('Y');?></p>
   </footer>
 </div><!-- container-->
 <?php wp_footer();?>
