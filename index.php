de0e9c1dc0411a6b258f125a6c0fdab8bb0b250c
vadar <szekelyferenc25@gmail.com>
2016-05-18 01:51:53 +0200

    Calling the night
    Saving a tag version here

--- index.php
+++ index.php
@@ -6,13 +6,16 @@ get_header();
 if (is_home()) {
 	query_posts('posts_per_page=4');
 };
-if (have_posts()) :
-	while (have_posts()) : the_post();?>
+if (have_posts()) :?>
+<div class="article_container">
+<?php	while (have_posts()) : the_post();?>
 	<article class="post">
 		<h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
 	  <?php the_content();?>
 	</article>
-<?php endwhile;
+<?php endwhile; ?>
+</div>
+<?php
 	else :
 		echo '<p>No content found.</p>';
 	endif;
