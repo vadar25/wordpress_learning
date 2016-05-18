dd3ae7b5f425d2c7e44292f39995d5db920aa144
vadar <szekelyferenc25@gmail.com>
2016-05-18 16:08:35 +0200

    dynamic content
    get template part in use

---
+++ content.php
@@ -0,0 +1,6 @@
+
+
+<article class="post">
+  <h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
+  <?php the_content();?>
+</article>
--- index.php
+++ index.php
@@ -3,21 +3,22 @@
 <?php
 
 get_header();
+
 if (is_home()) {
 	query_posts('posts_per_page=4');
 };
 if (have_posts()) :?>
 <div class="article_container">
-<?php	while (have_posts()) : the_post();?>
-	<article class="post">
-		<h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
-	  <?php the_content();?>
-	</article>
-<?php endwhile; ?>
+<?php	while (have_posts()) : the_post();
+get_template_part('content');
+
+ endwhile; ?>
 </div>
 <?php
 	else :
 		echo '<p>No content found.</p>';
 	endif;
+
+
 get_footer();
 ?>
