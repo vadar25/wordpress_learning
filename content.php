22abc0f60ec57556841d34d43da6f36889d05344
vadar <szekelyferenc25@gmail.com>
2016-05-18 17:06:14 +0200

    Throw everything into content.php
    Try to every code in content.php then just call it by individual phps

--- content.php
+++ content.php
@@ -1,6 +1,19 @@
 
-
-<article class="post">
-  <h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
-  <?php the_content();?>
-</article>
+<?php
+if (is_home()) {
+	query_posts('posts_per_page=4');
+};
+if (have_posts()) :?>
+<div class="article_container">
+  <?php	while (have_posts()) : the_post();?>
+  	<article class="post">
+  		<h2><a href="<?php the_permalink();?>"><?php echo the_title()?></a></h2>
+  	  <?php the_content();?>
+  	</article>
+  <?php endwhile; ?>
+</div>
+<?php
+	else :
+		echo '<p>No content found.</p>';
+	endif;
+?>
--- index.php
+++ index.php
@@ -4,20 +4,7 @@
 
 get_header();
 
-if (is_home()) {
-	query_posts('posts_per_page=4');
-};
-if (have_posts()) :?>
-<div class="article_container">
-<?php	while (have_posts()) : the_post();
 get_template_part('content');
-
- endwhile; ?>
-</div>
-<?php
-	else :
-		echo '<p>No content found.</p>';
-	endif;
 
 
 get_footer();
