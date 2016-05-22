b626cbc618072e72d0b7cb74a5c58afae3e21013
vadar <szekelyferenc25@gmail.com>
2016-05-22 02:15:11 +0200

    Heti hirek automatikus frissites beallitasa
    Csak az elmult egy het lesz benne a side news-ban

--- content.php
+++ content.php
@@ -43,13 +43,13 @@ if (have_posts()) :
 	endif;
 
 wp_reset_query();
-query_posts( 'cat=home' );
+query_posts( 'posts_per_page=100' );
 if (have_posts()) {?>
 	<aside id="side_news">
 	  <h4>Heti újdonságok:</h4>
 <?php 	while (have_posts()) {the_post();
-	if (in_category("Heti")) {?>
-<li><?php the_content(); ?></li>
+	if (in_category("Hírek") and array_reverse(getdate())[0] - get_the_date('U') < 604800 /*1 week in secs*/ ) {?>
+<p><?php the_content();?></p>
 <?php }} ?>
 </aside>
 <?php	} ?>
